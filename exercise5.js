e3040c6462812eb8c40b59eebb9f35efa0fb44b2
asepfirmans <asep.firmans@gmail.com>
2019-07-02 11:22:12 +0700

    tugasw2

---
+++ exercise5.js
@@ -0,0 +1,11 @@
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+//output JavaScript is awesome and I love it!
+console.log(word.concat(second));
+word= word + " " +second +" " +third +" " +fourth +" " +fifth +" " +sixth +" " +seventh
+console.log(word);
