6bc363f9d65eb8cdfe5db4c132303f5a1ff952e2
asepfirmans <asep.firmans@gmail.com>
2019-07-04 11:51:28 +0700

    tugasw2

---
+++ exercise7.js
@@ -0,0 +1,33 @@
+/* /var rows1=;
+for(var rows1=0; rows1<=5; i++){
+    i='*'
+    
+    console.log('rows1');
+    
+}*/
+var row1=5
+for (var i=0; i<row1; i++){
+    var hasil= ''
+    hasil +='*'
+    console.log(hasil);
+}
+var row2=5
+for (var i=0; i<row2; i++){
+    var hasil= '';
+  for  (var j=0; j<row2; j++){
+       
+        hasil +='*';
+    }
+    
+    console.log(hasil);
+}
+var row3=5
+for (var i=0; i<row3; i++){
+    var hasil= '*';
+  for  (var j=0; j<i; j++){
+       
+        hasil +='*';
+    }
+    
+    console.log(hasil);
+}
